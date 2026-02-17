9db407a04c778cb57ee86bf0d41df147458650df
zakariae-bouanane <zakariaebouanane@gmail.com>
2026-02-17 15:55:33 +0000

    adding JWT authentication with KeyCloack

--- src/main/java/novelis/formation/blog/config/SecurityConfig.java
+++ src/main/java/novelis/formation/blog/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package novelis.formation.blog.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -17,15 +18,19 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable()) // APIs don't use CSRF
                 .authorizeHttpRequests(auth -> auth
-//                        .requestMatchers("/api/auth/login").permitAll()
-                        .anyRequest().permitAll()
+                        .requestMatchers("/api/auth/login").permitAll()
+                        .anyRequest().authenticated()
+//                        .anyRequest().permitAll()
                 )
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS) // making requests stateless, so it doesnt change anything in server (independent requests)
                 )
                 .formLogin(form -> form.disable())
                 .httpBasic(basic -> basic.disable())
-                .logout(logout -> logout.disable());
+                .logout(logout -> logout.disable())
+
+                // set up for JWT with keycloack
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
 
         return http.build();
     }
